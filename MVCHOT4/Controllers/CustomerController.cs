d02ede87d7d76ba45a0b7030925cf32c596d680c
jackmerseal <jack_merseal@insideranken.org>
2024-03-15 11:27:29 -0500

    More progress on HOT4

--- MVCHOT4/Controllers/CustomerController.cs
+++ MVCHOT4/Controllers/CustomerController.cs
@@ -1,12 +1,72 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVCHOT4.Models;
 
 namespace MVCHOT4.Controllers
 {
 	public class CustomerController : Controller
 	{
+		private readonly AppointmentContext _context;
+		public CustomerController(AppointmentContext context)
+		{
+			_context = context;
+		}
+
 		public IActionResult Index()
 		{
 			return View();
+		}
+
+		[HttpGet]
+		public IActionResult Customers()
+		{
+			var customers = _context.Customers.OrderBy(c => c.Username).ToList();
+			return View(customers);
+		}
+
+		[HttpPost]
+        public IActionResult Customers(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Customers.Add(customer);
+                _context.SaveChanges();
+                return RedirectToAction("Customers");
+            }
+            else
+            {
+                return View(customer);
+            }
+        }
+
+		[HttpGet]
+		public IActionResult Add(int? id)
+		{
+			if (id.HasValue)
+			{
+				var customer = _context.Customers.Find(id.Value);
+				if (customer != null)
+				{
+					return View("Add", customer);
+				}
+			}
+			return View("Add");
+		}
+
+		[HttpPost]
+		public IActionResult Add(Customer customer)
+		{
+			if (ModelState.IsValid)
+			{
+				if (customer.CustomerId == 0)
+				{
+					_context.Customers.Add(customer);
+				}
+
+				_context.SaveChanges();
+				return RedirectToAction("Index");
+			}
+
+			return View("Add", customer);
 		}
 	}
 }
---
+++ MVCHOT4/Migrations/20240315161157_initial.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVCHOT4.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CustomerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Appointments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StartTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CustomerId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Appointments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Appointments_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "CustomerId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "CustomerId", "PhoneNumber", "Username" },
+                values: new object[] { 1, "123-456-7890", "Matt Turk" });
+
+            migrationBuilder.InsertData(
+                table: "Appointments",
+                columns: new[] { "Id", "CustomerId", "StartTime" },
+                values: new object[] { 1, 1, new DateTime(2024, 3, 15, 8, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_CustomerId",
+                table: "Appointments",
+                column: "CustomerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Appointments");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
